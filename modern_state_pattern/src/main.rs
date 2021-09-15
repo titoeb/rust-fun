73017f6b457301a6a90f7620c0556a71ad9b178e
titoeb <timtoebrock@googlemail.com>
2021-09-15 08:54:28 +0200

    Added modern state pattern.

---
+++ modern_state_pattern/src/main.rs
@@ -0,0 +1,16 @@
+use modern_state_pattern::Post;
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.reject();
+    
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
