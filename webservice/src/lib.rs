c9f71221a3c8906a2653611970b38a47f6ecc16e
titoeb <timtoebrock@googlemail.com>
2021-09-19 16:40:36 +0200

    finalized Webserive with multiple threads and gracefull shutdown.

---
+++ webservice/src/lib.rs
@@ -0,0 +1,88 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+pub struct Worker {
+    id: usize,
+    handle: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let handle = std::thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} got terminates", id);
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id: id,
+            handle: Some(handle),
+        }
+    }
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        /// Create a new Threadpool
+        ///
+        /// The size is the number of threads in the pool.
+        ///
+        /// # Panics
+        /// The `new` function will panic if the size is zero.
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(handle) = worker.handle.take() {
+                handle.join().unwrap();
+            }
+        }
+    }
+}
--- webservice/src/main.rs
+++ webservice/src/main.rs
@@ -1,19 +1,48 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use webservice::ThreadPool;
 
-fn handle_connection(mut stream: TcpStream){
+const GET: &[u8] = b"GET / HTTP/1.1\r\n";
+const SLEEP: &[u8] = b"GET /sleep HTTP/1.1\r\n";
+
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let (status_line, filename) = if buffer.starts_with(GET) {
+        // Get request.
+        (" HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(SLEEP) {
+        thread::sleep(Duration::from_secs(5));
+        (" HTTP/1.1 200 OK", "hello.html")
+    } else {
+        // Unkown request.
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming(){
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
